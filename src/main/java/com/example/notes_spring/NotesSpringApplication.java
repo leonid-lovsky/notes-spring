0632719363548fae8a45354f4c1b83505de7f171
Leonid Lovsky <leonid.lovsky@gmail.com>
2026-02-01 20:51:05 +0200

    Initial commit

---
+++ src/main/java/com/example/notes_spring/NotesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.notes_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notes_spring/NotesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notes_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
