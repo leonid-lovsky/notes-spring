ccfff0f7e80d5fc3f47799ee851d5bd6b6ac8dcd
Leonid Lovsky <leonid.lovsky@gmail.com>
2026-02-03 20:02:41 +0200

    Add Modulithic support and enhance application tests

--- src/test/java/com/example/notes_spring/NotesSpringApplicationTests.java
+++ src/test/java/com/example/notes_spring/NotesSpringApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.notes_spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
 
 @SpringBootTest
 class NotesSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        ApplicationModules.of(NotesSpringApplication.class).verify();
+    }
 }
