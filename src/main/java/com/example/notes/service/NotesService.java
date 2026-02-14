343ab535ca9b4ef6029348e9d8c23020482b3656
Leonid Lovsky <leonid.lovsky@gmail.com>
2026-02-14 12:56:07 +0200

    Add @Transactional annotation to NotesService for improved transaction management

--- src/main/java/com/example/notes/service/NotesService.java
+++ src/main/java/com/example/notes/service/NotesService.java
@@ -3,10 +3,12 @@ package com.example.notes.service;
 import com.example.notes.payload.NotesRead;
 import com.example.notes.payload.NotesWrite;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class NotesService {
 
     public NotesRead create(NotesWrite payload) {
