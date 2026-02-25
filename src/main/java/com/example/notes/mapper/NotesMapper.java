50ab82fe1b0985569b19532558314908022e1883
Leonid Lovsky <leonid.lovsky@gmail.com>
2026-02-25 16:01:33 +0200

    Update NotesMapper to enforce strict mapping and ignore ID during input conversion

--- src/main/java/com/example/notes/mapper/NotesMapper.java
+++ src/main/java/com/example/notes/mapper/NotesMapper.java
@@ -4,11 +4,18 @@ import com.example.notes.model.NotesEntity;
 import com.example.notes.payload.NotesInput;
 import com.example.notes.payload.NotesOutput;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(
+    componentModel = MappingConstants.ComponentModel.SPRING,
+    unmappedSourcePolicy = ReportingPolicy.ERROR,
+    unmappedTargetPolicy = ReportingPolicy.ERROR
+)
 public interface NotesMapper {
 
+    @Mapping(target = "id", ignore = true)
     NotesEntity notesInputToNotesEntity(NotesInput notesInput);
 
     NotesOutput notesEntityToNotesOutput(NotesEntity notesEntity);
