6c2992e4f9fc708888d92f759284dd208cbaf9fa
Leonid Lovsky <leonid.lovsky@gmail.com>
2026-03-27 23:58:36 +0300

    Refactor repository layer: extract `NoteRepository` implementation into new `JpaNoteRepositoryAdapter` and update `JpaNoteRepository` to only extend `JpaRepository` for improved separation of concerns.

--- src/main/java/com/example/note/persistence/jpa/JpaNoteRepository.java
+++ src/main/java/com/example/note/persistence/jpa/JpaNoteRepository.java
@@ -1,12 +1,11 @@
 package com.example.note.persistence.jpa;
 
-import com.example.note.application.service.NoteRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-interface JpaNoteRepository extends JpaRepository<JpaNoteEntity, UUID>, NoteRepository {
+interface JpaNoteRepository extends JpaRepository<JpaNoteEntity, UUID> {
 
 }
---
+++ src/main/java/com/example/note/persistence/jpa/JpaNoteRepositoryAdapater.java
@@ -0,0 +1,9 @@
+package com.example.note.persistence.jpa;
+
+import com.example.note.application.service.NoteRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+class JpaNoteRepositoryAdapater implements NoteRepository {
+
+}
