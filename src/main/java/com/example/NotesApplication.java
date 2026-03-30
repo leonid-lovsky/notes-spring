72b5a191a080cdf53aab436b26dbc68d11b93fc1
Leonid Lovsky <leonid.lovsky@gmail.com>
2026-03-31 00:40:26 +0300

    Remove outdated TODO comments in `NotesApplication` to improve code clarity.

--- src/main/java/com/example/NotesApplication.java
+++ src/main/java/com/example/NotesApplication.java
@@ -8,11 +8,7 @@ import org.springframework.modulith.Modulithic;
 @SpringBootApplication
 class NotesApplication {
 
-    // TODO: https://docs.spring.io/spring-modulith/reference/verification.html
-    // TODO: https://docs.spring.io/spring-modulith/reference/testing.html
     public static void main(String... args) {
         SpringApplication.run(NotesApplication.class, args);
     }
 }
-
-// TODO: Structure: Logical: application: Modules: module: Named interfaces: <<UNNAMED>>: list of interfaces
