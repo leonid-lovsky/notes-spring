8761dc2d9ea79b49defef9a1ef865d8216976059
Leonid Lovsky <leonid.lovsky@gmail.com>
2026-04-01 23:35:53 +0300

    Merge `NotesApplicationModulesTest` into `NotesApplicationTest` to consolidate and streamline module verification and context load testing.

--- src/test/java/com/example/NotesApplicationTest.java
+++ src/test/java/com/example/NotesApplicationTest.java
@@ -2,12 +2,13 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
 
 @SpringBootTest
 class NotesApplicationTest {
 
     @Test
     void contextLoads() {
-
+        ApplicationModules.of(NotesApplication.class).verify();
     }
 }
