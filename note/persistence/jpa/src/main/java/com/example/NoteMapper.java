bb86942e2fc5dc331b47134671dc2bb37856a1f4
Leonid Lovsky <leonid.lovsky@gmail.com>
2026-04-26 21:15:40 +0300

    Clean up build script by removing commented-out MapStruct and Lombok dependencies, and update `UserMapper` and `NoteMapper` to ignore unmapped targets.

--- note/persistence/jpa/src/main/java/com/example/NoteMapper.java
+++ note/persistence/jpa/src/main/java/com/example/NoteMapper.java
@@ -2,8 +2,12 @@ package com.example;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(
+    componentModel = MappingConstants.ComponentModel.SPRING,
+    unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 interface NoteMapper<Request, Response> extends CrudMapper<Request, Response, NoteEntity> {
 
 }
