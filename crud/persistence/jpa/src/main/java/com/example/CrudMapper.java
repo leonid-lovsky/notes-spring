990ae97586cf8993bcb5e8f53f382459e711f8d9
Leonid Lovsky <leonid.lovsky@gmail.com>
2026-04-26 21:04:55 +0300

    Clean up `CrudMapper` interface: remove commented-out `@Mapping` annotations.

--- crud/persistence/jpa/src/main/java/com/example/CrudMapper.java
+++ crud/persistence/jpa/src/main/java/com/example/CrudMapper.java
@@ -6,14 +6,11 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 
 interface CrudMapper<Request, Response, Entity> {
 
-    // TODO: @Mapping(target = "id", ignore = true)
     Entity toEntity(Request request);
 
-    // TODO: @Mapping(target = "id", ignore = true)
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void update(@MappingTarget Entity entity, Request request);
 
-    // TODO: @Mapping(target = "id", ignore = true)
     void replace(@MappingTarget Entity entity, Request request);
 
     Response toResponse(Entity entity);
