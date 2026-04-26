7e5f2904e605ed44a883f0283abb0c24e5ae42f1
Leonid Lovsky <leonid.lovsky@gmail.com>
2026-04-26 23:46:32 +0300

    Refactor repositories: replace `CrudRepository` with `JpaRepository` in `NoteRepository` and `UserRepository`.

--- note/persistence/jpa/src/main/java/com/example/NoteRepository.java
+++ note/persistence/jpa/src/main/java/com/example/NoteRepository.java
@@ -1,11 +1,11 @@
 package com.example;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-interface NoteRepository extends CrudRepository<NoteEntity, UUID> {
+interface NoteRepository extends JpaRepository<NoteEntity, UUID> {
 
 }
--- user/persistence/jpa/src/main/java/com/example/UserRepository.java
+++ user/persistence/jpa/src/main/java/com/example/UserRepository.java
@@ -1,11 +1,11 @@
 package com.example;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-interface UserRepository extends CrudRepository<UserEntity, UUID> {
+interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
 }
