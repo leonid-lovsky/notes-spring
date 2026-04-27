aac6b3362dcee1d17b63c508818b67b17c166323
Leonid Lovsky <leonid.lovsky@gmail.com>
2026-04-27 15:48:21 +0300

    Enhance `GlobalExceptionHandler`: extend `ResponseEntityExceptionHandler`, add `WebRequest` to `handleEntityNotFoundException`, and enable ProblemDetails in `application.properties`.

--- application/src/main/java/com/example/GlobalExceptionHandler.java
+++ application/src/main/java/com/example/GlobalExceptionHandler.java
@@ -4,13 +4,17 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-class GlobalExceptionHandler {
+class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    ProblemDetail handleEntityNotFoundException(EntityNotFoundException exception) {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    ProblemDetail handleEntityNotFoundException(EntityNotFoundException exception, WebRequest request) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, exception.getMessage());
     }
 }
