bc93a842c19fb1cecb32d32773d4127040cea97c
Leonid Lovsky <leonid.lovsky@gmail.com>
2026-05-16 16:33:47 +0300

    Migrate `auth-service` to use Spring Boot conventions, update Gradle configurations, rename application classes, and configure Java 17 toolchain.

--- application/src/main/java/com/example/application/MyApplication.java
+++ application/src/main/java/com/example/application/MyApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotesApplication {
+public class MyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NotesApplication.class, args);
+        SpringApplication.run(MyApplication.class, args);
     }
 }
