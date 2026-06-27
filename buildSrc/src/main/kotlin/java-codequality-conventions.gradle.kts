d3bf7e83a78c90dd07366ce0619199849be5fc64
Leonid Lovsky <leonid.lovsky@gmail.com>
2026-06-28 00:45:33 +0300

    Build: add Gradle configuration files for Spring Boot and code quality conventions

--- buildSrc/src/main/kotlin/java-codequality-conventions.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    id("java-javaformat-conventions")
-    id("java-errorprone-conventions")
-    id("java-jacoco-conventions")
-    id("java-jacoco-report-aggregation-conventions")
-}
--- buildSrc/src/main/kotlin/java-jacoco-report-aggregation-conventions.gradle.kts
+++
@@ -1,3 +0,0 @@
-plugins {
-    id("jacoco-report-aggregation")
-}
